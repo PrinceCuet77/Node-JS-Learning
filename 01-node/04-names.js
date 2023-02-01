885db67b44fecaae99be371cae4a4380281e0432
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2023-02-01 10:27:56 +0600

    Modules, OS, Path & File system

--- 01-node/04-names.js
+++ 01-node/04-names.js
@@ -5,3 +5,25 @@ const john = 'john'
 const peter = 'peter'
 
 module.exports = { john, peter }
+
+console.log(module)
+
+// Output:
+// Module {
+//   id: '.',
+//   path: '/home/prince-11209/Desktop/JS/Node-JS-Learning/01-node',
+//   exports: { john: 'john', peter: 'peter' },
+//   parent: null,
+//   filename: '/home/prince-11209/Desktop/JS/Node-JS-Learning/01-node/04-names.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     '/home/prince-11209/Desktop/JS/Node-JS-Learning/01-node/node_modules',
+//     '/home/prince-11209/Desktop/JS/Node-JS-Learning/node_modules',
+//     '/home/prince-11209/Desktop/JS/node_modules',
+//     '/home/prince-11209/Desktop/node_modules',
+//     '/home/prince-11209/node_modules',
+//     '/home/node_modules',
+//     '/node_modules'
+//   ]
+// }
