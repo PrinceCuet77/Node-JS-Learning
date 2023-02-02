5700eb8eccc3c885085b14af165a454d14b4b68b
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2023-02-03 01:48:10 +0600

    starting express js

--- 01-nodejs/12-http.js
+++ 01-nodejs/12-http.js
@@ -5,7 +5,10 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.end('Welcome to our webpage')
     } else if (req.url === '/about') {
-        res.end('Welcome to our about page')
+        // res.end('Welcome to our about page')
+
+        res.write('Welcome to our about page')
+        res.end()
     } else {
         res.end(
             `<h1>Opps!</h1>
@@ -16,4 +19,6 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen('5000')
+server.listen(5000, () => {
+    console.log('Server is running on 5000...')
+})
---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    // console.log(req.method) // GET, POST etc
+
+    const url = req.url // Output: '/'
+
+    // home page
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' }) // Provides header (meta-data)
+        res.write('<h1>home page</h1>')
+        res.end() // Indicates that communication is over.
+    } else if (url === '/about') {
+        // about page
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>')
+        res.end()
+    } else {
+        // 404
+        res.writeHead(404, { 'content-type': 'text/html' }) // Provides header (meta-data)
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000, () => {
+    console.log('Server is running on 5000...')
+})
